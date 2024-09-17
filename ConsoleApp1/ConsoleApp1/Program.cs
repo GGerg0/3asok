0b66a04a4dacbe3c5db92319f200be8b62202810
GGergo <gecsegergo006@gmail.com>
2024-09-17 11:29:00 +0200

    kesz

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = Convert.ToInt32(Console.ReadLine());
+            switch (szam)
+            {
+                case 3: Console.WriteLine("Osztható 3-mal"); break;
+                case 6: Console.WriteLine("Osztható 3-mal"); break;
+                case 9: Console.WriteLine("Osztható 3-mal"); break;
+                case 12: Console.WriteLine("Osztható 3-mal"); break;
+                case 15: Console.WriteLine("Osztható 3-mal"); break;
+                case 18: Console.WriteLine("Osztható 3-mal"); break;
+                default: Console.WriteLine("Nem osztható 3-mal"); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
